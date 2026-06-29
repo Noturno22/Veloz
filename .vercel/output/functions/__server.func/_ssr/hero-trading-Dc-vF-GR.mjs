cbabb3302870a940b863f8062d64b6dfd7db3fee
Noturno22 <jeronimo.samaina239898@gmail.com>
2026-06-29 16:27:24 +0100

    trigger deploy

--- .vercel/output/functions/__server.func/_ssr/hero-trading-Dc-vF-GR.mjs
+++
@@ -1,4 +0,0 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/hero-trading-Dc-vF-GR.js
-var hero_trading_default = "/assets/hero-trading-ti3AzGyG.jpg";
-//#endregion
-export { hero_trading_default as t };
